6e321d91f478db293f7cacfbaa4ee150c6f77f0d
Ashrahx <elfudanshi5@gmail.com>
2023-05-17 18:27:50 -0600

    Added favicon

--- index.php
+++ index.php
@@ -2,11 +2,12 @@
 <html lang="en">
 
 <head>
-	<meta charset="UTF-8" />
-	<meta http-equiv="X-UA-Compatible" content="IE=edge" />
-	<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+	<meta charset="UTF-8"/>
+	<meta http-equiv="X-UA-Compatible" content="IE=edge"/>
+	<meta name="viewport" content="width=device-width, initial-scale=1.0"/>
 	<title>BREKKI</title>
-	<link rel="stylesheet" href="./css/index.css" />
+	<link rel="stylesheet" href="./css/index.css"/>
+	<link rel="shortcut icon" href="./favicon.ico" type="image/x-icon">
 	<script src="https://kit.fontawesome.com/7af13692c6.js" crossorigin="anonymous"></script>
 	<script src="./script.js"></script>
 </head>
--- main.php
+++ main.php
@@ -11,11 +11,12 @@ if (!isset($_SESSION['username'])) {
 <html lang="en">
 
 <head>
-	<meta charset="UTF-8" />
-	<meta http-equiv="X-UA-Compatible" content="IE=edge" />
-	<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+	<meta charset="UTF-8"/>
+	<meta http-equiv="X-UA-Compatible" content="IE=edge"/>
+	<meta name="viewport" content="width=device-width, initial-scale=1.0"/>
 	<title>BREKKI</title>
-	<link rel="stylesheet" href="./css/main.css" />
+	<link rel="stylesheet" href="./css/main.css"/>
+	<link rel="shortcut icon" href="./favicon.ico" type="image/x-icon">
 	<script src="https://kit.fontawesome.com/7af13692c6.js" crossorigin="anonymous"></script>
 </head>
 
